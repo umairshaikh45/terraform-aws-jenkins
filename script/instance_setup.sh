1f0c9c3bf653de1d883648c0dd00d042217e3854
Umair <137191399+umairshaikh45@users.noreply.github.com>
2025-06-08 13:20:05 -0400

    Add S3 backup feature

--- script/instance_setup.sh
+++ script/instance_setup.sh
@@ -7,6 +7,8 @@ set -exu
 # Assign ec2 instance to proper clusters
 echo "ECS_CLUSTER=${cluster_name}" >> /etc/ecs/ecs.config
 
-
-
+# Configure backup cron job if enabled
+if [ "${backup_enabled}" = "true" ]; then
+  echo "${backup_schedule} root aws s3 sync /var/jenkins_home s3://${backup_bucket_name} --delete" > /etc/cron.d/jenkins_backup
+fi
 
